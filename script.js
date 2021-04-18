bcc4659e7721e27df317471754e65205b015c6bf
Betty Bialkowska <betty_1980_live@outlook.com>
2021-04-19 01:06:09 +0200

    einde oefening

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ console.log("voeg groente toe/vega variant");
 console.log("voeg groente en vlees/vis");
 console.log("bak op 200 graden ");
 
-};
+}
 makingPizzaProcess();
 makingPizzaProcess();
 
@@ -22,3 +22,28 @@ console.log("gebruik vis, groenten etc..doe maar wat");
 makingSushiProcess();
 makingSushiProcess();
 makingSushiProcess();
+
+//oplossing
+/* Step 1
+const makePizza = function() {
+    console.log("Roll dough");
+    console.log("Spread tomato sauce");
+    console.log("Grate cheese");
+    console.log("Bake in oven");
+};
+
+makePizza();
+makePizza();
+makePizza();
+
+// Step 2
+const makeSushi = function() {
+    console.log("Boil rice");
+    console.log("Cut avocado and fish");
+    console.log("Roll sushi");
+};
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();*/
