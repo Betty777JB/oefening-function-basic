da5024bf3bedd648f27a267957fbe5d661f5470c
Betty Bialkowska <betty_1980_live@outlook.com>
2021-04-19 01:02:13 +0200

    einde oefening-function-basic-js

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+function makingPizzaProcess() {
+console.log("maak een pizzadeeg");
+console.log("doe de tomatensaus");
+console.log("voeg groente toe/vega variant");
+console.log("voeg groente en vlees/vis");
+console.log("bak op 200 graden ");
+
+};
+makingPizzaProcess();
+makingPizzaProcess();
+
+
+function makingSushiProcess() {
+console.log("was de rijst");
+console.log("kook de rijst");
+console.log("laat de rijst afkolen");
+console.log("gebruik vis, groenten etc..doe maar wat");
+}
+makingSushiProcess();
+makingSushiProcess();
+makingSushiProcess();
